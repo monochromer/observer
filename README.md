c0806d45cc70193aca3e87356877950b7d77b32e
monochromer <monochromer@mail.ru>
2016-09-01 17:31:03 +0500

    Добалены модуль для ES6

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Observer
 Реализация шаблона "Издатель-подписчик" (или "Наблюдатель")
+
+Пример использования
+```javascript
+import Observer from './observer';
+
+let Person = {
+    name: 'John',
+    getName() {
+        console.log((this.name || this), arguments);
+    }
+};
+
+
+let func = Person.getName.bind(Person);
+
+Observer.on('data:load', func);
+Observer.emit('data:load', 'some string', 'some value');
+Observer.off('data:load', func);
+Observer.emit('data:load', 'some string');
+```
--- observer.js
+++ observer.js
@@ -1,95 +1,26 @@
-(function (root, factory) {
-
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else if (typeof exports === "object") {
-        module.exports = factory();
-    } else {
-        root.Observer = factory();
+export default {
+    on(type, fn) {
+        this._events = this._events || {};
+        this._events[type] = this._events[type] || [];
+        this._events[type].push(fn);
+        return this;
+    },
+  
+    off(type, fn) {
+        let index, events = this._events[type];
+        if (events && (index = events.indexOf(fn)) > -1) events.splice(index, 1);
+        if (this._events[type].length <= 0) delete this._events[type];
+        return this;
+    },
+  
+    once(type, fn) {
+        let handler = (...args) => { this.off(type, handler); fn(...args); };
+        this.on(type, handler);
+        return this;
+    },
+  
+    emit(type, ...args) {
+        if (this._events[type]) this._events[type].forEach(fn => fn(...args));
+        return this;
     }
-
-}(this, function () {
-
-    'use strict';
-
-    var exports = {},
-        events = {},
-        splitter =  /\s+/;
-
-    function on(types, fn, context) {
-        var type;
-
-        types = types.split(splitter);
-        type = types.pop();
-        context = context || this;
-
-        while (type) {
-            events[type] = events[type] || [];
-            events[type].push({
-                context: context,
-                callback: fn
-            });
-            type = types.pop();
-        }
-
-        return this;
-    };
-
-    function off(types, fn) {
-        var type,
-            index;
-
-        types = types.split(splitter);
-        type = types.pop();
-
-        while (type && type in events) {
-            index = events[type].indexOf(fn);
-            events[type].splice(index, 1);
-            if(events[type].length === 0) {
-                delete events[type];
-            }
-            type = types.pop();
-        }
-
-        return this;
-    };
-
-    function once(types, fn, context) {
-        var self = this;
-
-        function handler() {
-            self.off(types, handler);
-            fn.apply(context, arguments);
-        }
-
-        this.on(types, handler, context);
-
-        return this;
-    };
-
-    function emit(types) {
-        var type, args, e,
-            subscription,
-            len;
-
-        types = types.split(splitter);
-        type = types.pop();
-        args = Array.prototype.slice.call(arguments, 1);
-
-        while(type && (type in events)) {
-            e = events[type];
-            for (len = e.length; len > 0; len -= 1) {
-                subscription = e[len - 1];
-                subscription.callback.apply(subscription.context, args);
-            }
-            type = types.pop();
-        }
-    };
-
-    exports.on = on;
-    exports.off = off;
-    exports.once = once;
-    exports.emit = emit;
-
-    return exports;
-}));
+};
